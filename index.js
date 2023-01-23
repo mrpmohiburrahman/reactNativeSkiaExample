74cc352f9de2718339731c7deece164d5399f5e2
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-01-23 21:23:58 +0600

    all files added

---
+++ index.js
@@ -0,0 +1,16 @@
+// /**
+//  * @format
+//  */
+
+// import { AppRegistry } from "react-native";
+// // import App from './src/App';
+// import App from "./App";
+// import { name as appName } from "./app.json";
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import { registerRootComponent } from "expo";
+import App from "./src/App";
+// import App from "./App";
+
+registerRootComponent(App);
